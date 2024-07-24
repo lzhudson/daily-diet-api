534519824161abec5b2f0c744615df1c5ee1a13b
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-24 10:07:18 -0300

    feat: create type for table users

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      username: string
+    }
+  }
+}
