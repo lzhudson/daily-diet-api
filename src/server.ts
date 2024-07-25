dfa0d2f1e517415d9a927a8df7d60c72f4f20745
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-25 10:26:35 -0300

    feat: register meals routes

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import fastify from 'fastify'
 import { usersRoutes } from './routes/users'
 import fastifyCookie from '@fastify/cookie'
 import { authRoutes } from './routes/auth'
+import { mealsRoutes } from './routes/meals'
 
 export const app = fastify()
 
 app.register(fastifyCookie)
-app.register(usersRoutes, { prefix: 'users' })
 app.register(authRoutes)
+app.register(usersRoutes, { prefix: 'users' })
+app.register(mealsRoutes, { prefix: 'meals' })
 
 app
   .listen({
